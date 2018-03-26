09424107d3946c88ca8de9aadff9dcba4a7ddf27
Pedro Henrique Nascimento Vieira <pedrohnv@hotmail.com>
2018-03-26 11:02:22 -0300

    Correção impedância longitudinal
    Nas expressões de ZL, ZLp = +jw..., ZLm = -jw...

--- EletrodosFinitos.m
+++ EletrodosFinitos.m
@@ -7,7 +7,7 @@
 (* ::Program:: *)
 (*Autor: Pedro Henrique Nascimento Vieira*)
 (*Criado em: 26 de Outubro de 2017*)
-(*\[CapitalUAcute]ltima altera\[CCedilla]\[ATilde]o: 28 de Novembro de 2017*)
+(*\[CapitalUAcute]ltima altera\[CCedilla]\[ATilde]o: 23 de Marco de 2018*)
 
 
 (* ::Text:: *)
@@ -143,7 +143,7 @@ Block[{Ls,k1,k2},
 	Ls = Norm[eletrodo[[1]] - eletrodo[[2]]];
 	k1 = eletrodo[[3]]/Ls;
 	k2 = Sqrt[1. + k1^2.];
-	Return[(I \[Omega] \[Mu])/(2. \[Pi]) (Log [(k2 + 1.)/k1] - k2 + k1) + eletrodo[[4]]]
+	Return[(I \[Omega] \[Mu] Ls)/(2. \[Pi]) (Log [(k2 + 1.)/k1] - k2 + k1) + eletrodo[[4]]]
 ]
 ImpedanciaLongitudinalPropria::usage=
 "Calcula a imped\[AHat]ncia longitudinal pr\[OAcute]pria de um eletrodo considerando-o imerso em meio infinito, isotr\[OAcute]pico e homog\[EHat]neo.
@@ -165,7 +165,7 @@ Block[{ds,dr,Ls,Lr,cos\[Phi],int},
 		int = NIntegrate[Integrando[emissor,receptor,\[Gamma],t,Ls],{t,0.,1.},Compiled->False];,
 		int = integral; (*else*)
 	];
-	Return[(I \[Omega] \[Mu] cos\[Phi])/(4. \[Pi] Ls) int]
+	Return[(-I \[Omega] \[Mu] cos\[Phi])/(4. \[Pi]) int]
 ]
 ImpedanciaLongitudinalMutua::usage=
 "Calcula a imped\[AHat]ncia longitudinal m\[UAcute]tua entre dois eletrodos considerando-os imersos em meio infinito, isotr\[OAcute]pico e homog\[EHat]neo.
@@ -180,6 +180,10 @@ Argumentos:
 
 (* ::Section:: *)
 (*Imped\[AHat]ncia Total*)
+
+
+(* ::Subsection::Closed:: *)
+(*Versao original*)
 
 
 ImpedanciaEletrodos[eletrodos_,\[Gamma]_,c_,\[Mu]_,\[Omega]_,simplificado_:False]:=
@@ -225,7 +229,7 @@ IncluirImagensImpedancia[zt_,zl_,eletrodos_,imagens_,\[Gamma]_,c_,\[Mu]_,\[Omega
 Block[{ZT=zt,ZL=zl,numEletrodos,Ls,integral,\[Eta]},
 	numEletrodos = Dimensions[eletrodos][[1]];
 	(*adicionar mutua com imagem \[AGrave] Z pr\[OAcute]pria*)
-	Do[
+	(*Do[
 		If[imagens[[i]] === Null, Continue[]];
 		Ls = Norm[eletrodos[[i,1]] - eletrodos[[i,2]]];
 		integral = NIntegrate[Integrando[imagens[[i]],eletrodos[[i]],\[Gamma],t,Ls,simplificado],{t,0.,1.},Compiled->False];
@@ -237,7 +241,7 @@ Block[{ZT=zt,ZL=zl,numEletrodos,Ls,integral,\[Eta]},
 		ZL[[i,i]] += (1 - coefReflexao) ImpedanciaLongitudinalMutua[imagens[[i]],eletrodos[[i]],\[Gamma],\[Mu],\[Omega],integral];
 		,{i,numEletrodos}
 	];
-	If[numEletrodos === 1, Return[{ZT,ZL}]];
+	If[numEletrodos === 1, Return[{ZT,ZL}]];*)
 	(*adicionar efeito da imagem \[AGrave] Z m\[UAcute]tua*)
 	Do[
 		If[imagens[[i]] === Null || imagens[[k]] === Null, Continue[]];
@@ -251,7 +255,7 @@ Block[{ZT=zt,ZL=zl,numEletrodos,Ls,integral,\[Eta]},
 		ZL[[i,k]] += (1 - coefReflexao) ImpedanciaLongitudinalMutua[imagens[[i]],eletrodos[[k]],\[Gamma],\[Mu],\[Omega],integral];
 		ZT[[k,i]] = ZT[[i,k]];
 		ZL[[k,i]] = ZL[[i,k]];
-		,{i,1,numEletrodos},{k,i+1,numEletrodos}
+		,{i,1,numEletrodos},{k,i,numEletrodos}
 	];
 	Return[{ZT,ZL}]
 ]
@@ -270,6 +274,106 @@ Argumentos:
 		padr\[ATilde]o = 1.0
 	simplificado: opicional, Booleano se considera Exp[-\[Gamma] \[Eta]] constante ou n\[ATilde]o durante a integra\[CCedilla]\[ATilde]o.
 		padr\[ATilde]o = False
+-------
+Retorna:
+	ZT: matriz de imped\[AHat]ncia transveral com efeito das imagens incluso;
+	ZL: matriz de imped\[AHat]ncia longitudinal com efeito das imagens incluso.";
+
+
+(* ::Subsection:: *)
+(*Versao simplificada*)
+
+
+(* ::Text:: *)
+(**Nota sobre os argumentos**)
+(*integral: e uma matriz cujo elemento [[i,k]] e o valor da integral do eletrodo emissor i em relacao ao receptor k.*)
+(*etam: e a matriz de distancias dos pontos medios dos eletrodos. Os elementos da diagonal sao o comprimento dos eletrodos.*)
+(*etai: matriz de distancias dos pontos medios dos eletrodos ao ponto medio das imagens.*)
+
+
+ImpedanciaEletrodosSimples[eletrodos_,\[Gamma]_,c_,\[Mu]_,\[Omega]_,integral_,etam_]:=
+Block[{ZT,ZL,numEletrodos,Ls,intg},
+	numEletrodos = Dimensions[eletrodos][[1]];
+	ZT = Table[ImpedanciaTransversalPropria[e,c],{e,eletrodos}];
+	ZT = DiagonalMatrix[ZT];
+	ZL = Table[ImpedanciaLongitudinalPropria[e,\[Mu],\[Omega]],{e,eletrodos}];
+	ZL = DiagonalMatrix[ZL];
+	If[numEletrodos === 1, Return[{ZT,ZL}]];
+	Do[
+		Ls = etam[[i,i]];
+		intg = Exp[-\[Gamma] etam[[i,k]]] integral[[i,k]];
+		ZT[[i,k]] = ImpedanciaTransversalMutua[eletrodos[[i]],eletrodos[[k]],\[Gamma],c,intg];
+		ZL[[i,k]] = ImpedanciaLongitudinalMutua[eletrodos[[i]],eletrodos[[k]],\[Gamma],\[Mu],\[Omega],intg];
+		ZT[[k,i]] = ZT[[i,k]];
+		ZL[[k,i]] = ZL[[i,k]];
+		,{i,1,numEletrodos},{k,i+1,numEletrodos}
+	];
+	Return[{ZT,ZL}]
+]
+ImpedanciaEletrodosSimples::usage=
+"Calcula as matrizes de imped\[AHat]ncia longitudinal e transvesal do conjunto de eletrodos considerando-os imersos em meio homog\[EHat]neo.
+Versao otimizada para caso simplificado.
+Argumentos:
+	eletrodos: lista de listas dos eletrodos '{{ponto_inicial}, {ponto_final}, raio, imped\[AHat]ncia_interna_total}';
+	\[Gamma]: constante de propaga\[CCedilla]\[ATilde]o do meio;
+	c: condutividade el\[EAcute]trica complexa do meio (\[Sigma] + I \[Omega] \[Epsilon]);
+	\[Mu]: permeabilidade magn\[EAcute]tica do meio;
+	\[Omega]: frequ\[EHat]ncia angular;
+	integral: matriz do valor das integrais;
+	etam: matriz de distancias medias entre eletrodos;
+-------
+Retorna:
+	ZT: matriz de imped\[AHat]ncia transveral;
+	ZL: matriz de imped\[AHat]ncia longitudinal.";
+
+
+IncluirImagensImpedanciaSimples[zt_,zl_,eletrodos_,imagens_,\[Gamma]_,c_,\[Mu]_,\[Omega]_,integral_,etam_,etai_,coefReflexao_:1.0]:=
+Block[{ZT=zt,ZL=zl,numEletrodos,Ls,intg},
+	numEletrodos = Dimensions[eletrodos][[1]];
+	(*adicionar mutua com imagem \[AGrave] Z pr\[OAcute]pria*)
+	(*Do[
+		If[imagens[[i]] === Null, Continue[]];
+		Ls = Norm[eletrodos[[i,1]] - eletrodos[[i,2]]];
+		integral = NIntegrate[Integrando[imagens[[i]],eletrodos[[i]],\[Gamma],t,Ls,simplificado],{t,0.,1.},Compiled->False];
+		If[simplificado,
+			\[Eta] = Norm[(imagens[[i,1]] + imagens[[i,2]])/2 - (eletrodos[[i,1]] + eletrodos[[i,2]])/2];
+			integral = integral Exp[-\[Gamma] \[Eta]];
+		];
+		ZT[[i,i]] += coefReflexao ImpedanciaTransversalMutua[imagens[[i]],eletrodos[[i]],\[Gamma],c,integral];
+		ZL[[i,i]] += (1 - coefReflexao) ImpedanciaLongitudinalMutua[imagens[[i]],eletrodos[[i]],\[Gamma],\[Mu],\[Omega],integral];
+		,{i,numEletrodos}
+	];
+	If[numEletrodos === 1, Return[{ZT,ZL}]];*)
+	(*adicionar efeito da imagem \[AGrave] Z m\[UAcute]tua*)
+	Do[
+		If[imagens[[i]] === Null || imagens[[k]] === Null, Continue[]];
+		Ls = etam[[i,i]];
+		intg = Exp[-\[Gamma] etai[[i,k]]] integral[[i,k]];
+		ZT[[i,k]] += coefReflexao ImpedanciaTransversalMutua[imagens[[i]],eletrodos[[k]],\[Gamma],c,intg];
+		ZL[[i,k]] += (1 - coefReflexao) ImpedanciaLongitudinalMutua[imagens[[i]],eletrodos[[k]],\[Gamma],\[Mu],\[Omega],intg];
+		ZT[[k,i]] = ZT[[i,k]];
+		ZL[[k,i]] = ZL[[i,k]];
+		,{i,1,numEletrodos},{k,i,numEletrodos}
+	];
+	Return[{ZT,ZL}]
+]
+IncluirImagensImpedanciaSimples::usage=
+"Adiciona \[AGrave]s matrizes de imped\[AHat]ncia longitudinal e transvesal o efeito das imagens. Se houver m\[UAcute]ltiplas imagens para um mesmo eletrodo, use esta fun\[CCedilla]\[ATilde]o recursivamente.
+Versao otimizada para caso simplificado.
+Argumentos:
+	zt: matriz de imped\[AHat]ncia transveral;
+	zl: matriz de imped\[AHat]ncia longitudinal;
+	eletrodos: lista de listas dos eletrodos '{{ponto_inicial}, {ponto_final}, raio, imped\[AHat]ncia_interna_total}';
+	imagens: lista de listas, onde imagens[[i]] \[EAcute] a imagem de eletrodos[[i]] (se n\[ATilde]o houver imagem para ele, ent\[ATilde]o ela deve ser Null. Neste caso, inclusive, o eletrodo n\[ATilde]o receber\[AAcute] influ\[EHat]ncia de qualquer imagem);
+	\[Gamma]: constante de propaga\[CCedilla]\[ATilde]o do meio;
+	c: condutividade el\[EAcute]trica complexa do meio (\[Sigma] + I \[Omega] \[Epsilon]);
+	\[Mu]: permeabilidade magn\[EAcute]tica do meio;
+	\[Omega]: frequ\[EHat]ncia angular;
+	integral: matriz do valor das integrais;
+	etam: matriz de distancias medias entre eletrodos;
+	etai: matriz de distancias medias entre eletrodos e imagens;
+	coefReflexao: opicional, o coeficiente de reflex\[ATilde]o para as imagens;
+		padr\[ATilde]o = 1.0
 -------
 Retorna:
 	ZT: matriz de imped\[AHat]ncia transveral com efeito das imagens incluso;
